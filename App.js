3104a99c9890841d3e66cab297b07b307143528f
swonb <47047914+swonb@users.noreply.github.com>
2020-07-09 18:33:23 -0400

    Added Images

--- App.js
+++ App.js
@@ -1,48 +1,10 @@
 // import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  SafeAreaView,
-  Button,
-  Platform,
-  StatusBar,
-  View,
-  Dimensions,
-} from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  // console.log(Dimensions.get("screen"));
-  // console.log(useDimensions());
-  // console.log(useDeviceOrientation());
-
-  // const orientation = useDeviceOrientation();
-  const { landscape } = useDeviceOrientation();
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
-        }}
-      ></View>
-      {/* <StatusBar style="light" /> */}
-    </SafeAreaView>
-  );
+  return <ViewImageScreen />;
+  // return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    // paddingTop: Expo.Constants.statusBarHeight,
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
-});
